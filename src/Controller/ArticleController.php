f2c8e4417488601dc7bad9b0260be0ad0f938ace
Vasyl Boychuk <vasyaboychuk@gmail.com>
2018-05-28 20:15:12 +0300

    added doctrine and persist Article entity in db

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,6 +13,16 @@ class ArticleController extends Controller {
      * @Method({"GET"})
      */
     public function index() {
-        return $this->render('articles/index.html.twig');
+//        $mg = $this->getDoctrine()->getManager();
+//        $a = new Article();
+//        $a->setTitle('news 1');
+//        $a->setBody('news 1 body; news 1 body; news 1 body; news 1 body;');
+//        $mg->persist($a);
+//        $mg->flush();
+
+        $articles = $this->getDoctrine()->
+        getRepository(Article::class)->findAll();
+
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Body;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->Body;
+    }
+
+    public function setBody(string $Body): self
+    {
+        $this->Body = $Body;
+
+        return $this;
+    }
+}
