0c7af757667796e59b695df74887ef40d324b0c3
Vasyl Boychuk <vasyaboychuk@gmail.com>
2018-05-28 19:28:25 +0300

    introduce twig template

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController
-{
-    public function index()
-    {
-        $number = mt_rand(0, 100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+class ArticleController extends Controller {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index() {
+        return $this->render('articles/index.html.twig');
     }
 }
